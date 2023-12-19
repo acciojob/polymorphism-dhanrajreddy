01efc5007dbf8a42c266cb2c3dc5ca81631ca851
dhanrajreddy <dhanrajkutwade7@gmail.com>
2023-12-19 12:25:38 +0530

    code written

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
+        public int product(int x, int y){
+            return x+y;
+        }
 
+        public int product(int x, int y,int z){
+            return x+y;
+        }
+
+        public double product(double x, double y){
+            return x+y;
+        }
+    }
+    public static void main(String[] args) {
+        Product p= new Product();
+        int ans=p.product(10,20);
+        int ans1=p.product(10,20,30);
+        double ans2=p.product(10.0,20.0);
+
+        System.out.println(ans +" "+ans1+" "+ans2);
+    }
 }
